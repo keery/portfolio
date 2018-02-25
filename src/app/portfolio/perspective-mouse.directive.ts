2273cbac6a445e66d9b4de2268d570b0f3dc4505
ESNAULT-Guillaume <mon-compte1131@live.fr>
2018-02-25 19:22:09 +0100

    Correction perspective-mouse pour avoir toujours le même degré de rotation selon la taille de l'écran + gestion des sorties du container

--- src/app/portfolio/perspective-mouse.directive.ts
+++ src/app/portfolio/perspective-mouse.directive.ts
@@ -1,51 +1,61 @@
-import { Directive, ElementRef, Renderer, HostListener } from '@angular/core';
+import { Directive, ElementRef, Renderer2, HostListener, AfterViewInit } from '@angular/core';
 
 @Directive({ selector: '[perspective]' })
-export class PerspectiveMouseDirective {
+export class PerspectiveMouseDirective implements AfterViewInit  {
 
-    x:number;
-    y:number;
+    x:number = 0;
+    y:number = 0;
     perspectiveX:number = 3;
-    perspectiveY:number = 20;
+    perspectiveY:number = 5;
+    container:any = null;
 
-    constructor(private el: ElementRef, private renderer: Renderer) {
-        this.x = 0;
-        this.y = 0;
+    constructor(private el: ElementRef, private renderer: Renderer2) {}
 
+    ngAfterViewInit(){
         const persX = this.el.nativeElement.getAttribute("perspectiveX");
         const persY = this.el.nativeElement.getAttribute("perspectiveY");
 
         if(persX) this.perspectiveX = persX; 
         if(persY) this.perspectiveY = persY;
 
-        this.renderer.setElementStyle(this.el.nativeElement.parentElement, 'perspective', "500px");
-        this.renderer.setElementStyle(this.el.nativeElement.parentElement, 'overflow', "hidden");
-        this.renderer.setElementStyle(this.el.nativeElement.parentElement, 'transform-style', "preserve-3d");
+        this.container = this.el.nativeElement.querySelector('.perspective-container');
+        console.log(this.container);
+        if(!this.container) console.log('Pas de container pour (perspective-mouse)');
+
+        this.renderer.setStyle(this.el.nativeElement, 'perspective', "500px");
+        this.renderer.setStyle(this.el.nativeElement, 'perspective', "500px");
+        this.renderer.setStyle(this.el.nativeElement, 'overflow', "hidden");
+        this.renderer.setStyle(this.el.nativeElement, 'transform-style', "preserve-3d");     
     }
 
     @HostListener('mouseenter') onMouseEnter() {
         // renderer.setElementClass(el, 'class1', false); // remove class1
         // renderer.setElementClass(el, 'class2', true); // add class2
-        this.el.nativeElement.classList.remove('leave');
+        this.container.classList.remove('leave');
     }
 
     @HostListener('mouseleave') onMouseLeave() {
-        this.el.nativeElement.classList.add('leave');
+        this.renderer.addClass(this.container, 'leave');
+    
     }
 
     @HostListener('mousemove', ['$event']) onMousemove(event: MouseEvent) {
 
+        //Position de la souris sur l'element qui possède la directive
         this.x = event.screenX - this.el.nativeElement.offsetLeft;
         this.y = event.screenY - this.el.nativeElement.offsetTop;
 
-        let largeur:number = this.el.nativeElement.offsetWidth;
-        let hauteur:number = this.el.nativeElement.offsetHeight;
+        //Largeur et hauteur de l'element qui possède la directive
+        let largeur:number = this.el.nativeElement.offsetWidth,
+            hauteur:number = this.el.nativeElement.offsetHeight;
 
-        let pourcentX = ((this.x / largeur * 100)/100)-0.5,
-            pourcentY = ((this.y / largeur * 100)/100)-0.5;
-
-        let resultX = pourcentX * (this.perspectiveX * 2),
-            resultY = -(pourcentY * this.perspectiveY * 2);
+        //Pourcentage de la position de la souris, Gauche = -50%, Milieu = 0%, Droite = 50%
+        let pourcentX = ((this.x * 100 / largeur)-50)*0.01,
+            pourcentY = ((this.y * 100 / hauteur)-50)*0.01;            
+        
+        //Ajout de l'indice de perspective 
+        let resultX = pourcentX * this.perspectiveX,
+            resultY = -(pourcentY * this.perspectiveY)+1;
 
         this.setTransform(resultX, resultY);
   
@@ -54,6 +64,6 @@ export class PerspectiveMouseDirective {
     private setTransform(x:number, y:number)
     {
         let style = "rotateY("+(x)+"deg) rotateX("+(y)+"deg)";
-        this.renderer.setElementStyle(this.el.nativeElement, 'transform', style);
+        this.renderer.setStyle(this.container, 'transform', style);
     }
 }
