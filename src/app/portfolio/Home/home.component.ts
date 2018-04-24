d1659a4ae79da72006d45cd8263c7cfda77bad36
keery <mon-compte1131@live.fr>
2018-04-24 10:44:48 +0200

    Gestion vidéo home ne mobile

--- src/app/portfolio/Home/home.component.ts
+++ src/app/portfolio/Home/home.component.ts
@@ -12,15 +12,17 @@ export class HomeComponent implements OnInit
 {
 	@ViewChild('videoSmoke') video: ElementRef;
 	objLoaderStatus:boolean =false;
+	isMobile = false;
 
 	constructor(private loaderService: LoaderService,private elRef: ElementRef, private renderer: Renderer2) {}
 
 	ngOnInit():void{
+		if(window.innerWidth <= 500) this.isMobile = true;
 		this.loaderService.loaderStatus.subscribe((val: boolean) => {
 			//Si le loader etait affiché et qu'on lui dit de le cacher ou si il etait cache et qu'on ne veut pas l'afficher
             if((this.objLoaderStatus && !val) || (!this.objLoaderStatus && !val)) {
             	this.renderer.addClass(this.elRef.nativeElement, "enter");
-            	this.video.nativeElement.play();
+            	if(!this.isMobile) this.video.nativeElement.play();
             }
             this.objLoaderStatus = val;
         }); 
