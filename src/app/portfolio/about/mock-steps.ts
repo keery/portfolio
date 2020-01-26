318b38dcb9850395931ab0b1686bea3729917312
keery <mon-compte1131@live.fr>
2020-01-26 18:50:15 +0100

    feat: translation page about

--- src/app/portfolio/about/mock-steps.ts
+++ src/app/portfolio/about/mock-steps.ts
@@ -4,25 +4,16 @@ export const STEPS: { [key: number]: Step } = {
 	1 : {
 		idSlide : 1,
 		delay: 3000,
-		titre: "A propos de moi",
-		subInfo: {			
-			Email : '<a href="mailto:contact@guillaumeesnault.fr" class="slide-link"><div class="default">contact@guillaumeesnault.fr</div><div>contact@guillaumeesnault.fr</div></a>',
-		},
-		description: "Hello, je m'appelle Guillaume ESNAULT, j'ai 23 ans, je vis en région parisienne et suis actuellement en étude à l'école ESGI (BAC +3). J'ai commencé en autodidacte le développement web en 2013 jusqu'à aujourd'hui où j'ai pu apprendre des technos comme Symfony ou Angular, je cherche dorénavant à me spécialiser dans le développement front pour lequel j'ai un penchant significatif."
-	},
-	2 : {
-		idSlide : 2,
-		delay: 5000,
 		titre: "GEN Contact",
-		subInfo: {
+		subInfo: {			
 			Période : "2015 - 2017",
 			Site : "<a href='http://www.gencontact.fr' target='_blank' class='slide-link'><div class='default'>gencontact.fr</div><div>gencontact.fr</div></a>",
 			Poste : "Full stack"
 		},
-		description: "GEN Contact est une agence web vendant de la création de sites et des prestations en print. Premier emploi dans le développement j'ai pu me faire la main sur tous les aspects du métier, front, back, graphisme et ainsi enrichir et renforcer mes compétences. Mes tâches principales étaient de l'intégration et le développement de modules."
+		description: "Hi, my name is Guillaume ESNAULT, 25 years old, JS (fan) developer from Paris with huge preference for React. I started to learn development by myself in 2013 until my first experience. It was in GEN Contact a web agency selling websites and print services. My first job as developer I discovered a lot of aspects about this profession, back, front, how to manage a database, how to create a graphic model. My main tasks was websites integration and module development in PHP/jQuery."
 	},
-	4 : {
-		idSlide : 3,
+	2 : {
+		idSlide : 2,
 		delay: 2000,
 		titre: "Neftis",
 		subInfo: {
@@ -30,6 +21,17 @@ export const STEPS: { [key: number]: Step } = {
 			Entreprise : "<a href='http://www.neftis.fr' target='_blank' class='slide-link'><div class='default'>neftis.fr</div><div>neftis.fr</div></a>",
 			Poste : "Full stack"
 		},
-		description: "Neftis est une agence web ayant développé son CMS nommé Flexit, mon activité première a été de développer des fonctionnalités pour ce CMS. Mon poste majoritairement orienté back, j'y ai pu néanmoins réaliser quelques sites internets et interfaces graphiques."
+		description: "Neftis is a web agency that had developed a home made CMS named FLEXIT, my main activity was to develop new features for it. Mainly back oriented, but I developed websites for customers."
+	},
+	4 : {
+		idSlide : 3,
+		delay: 2000,
+		titre: "Sewan",
+		subInfo: {
+			Période : "2018 - 2020",
+			Entreprise : "<a href='http://www.sewan.fr' target='_blank' class='slide-link'><div class='default'>sewan.fr</div><div>sewan.fr</div></a>",
+			Poste : "Full stack JS"
+		},
+		description: "Sewan is a telecommunication company where I worked on many projects in order to make life easier for employees. My first experience in a big company with more than 80 developers, that offers the possibility to share about a lot of different subjects. I worked on projects using React, Node, GraphQL, MongoDB, with a SCRUM organization."
 	}
 };
